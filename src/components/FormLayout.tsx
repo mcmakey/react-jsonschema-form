22add71ee2b00b0d06406eaa83870e5f112f87f8
makey <vadimmakievsky@gmail.com>
2021-11-12 14:04:33 +0300

    ex: type-objects

--- src/components/FormLayout.tsx
+++ src/components/FormLayout.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import JSONSchemaForm from "@rjsf/core";
+import { ISubmitEvent, IChangeEvent } from "@rjsf/core";
 import { JSONSchema7 } from "json-schema";
 
 const Form = JSONSchemaForm;
 
 const schema: JSONSchema7 = {
-    title: "My form",
-    // description: "My description",
-    type: "string",
+    type: "object",
+    properties: {
+        foo: { type: "string" },
+        bar: { type: "string" },
+    },
 };
 
-const FormLayout: React.FC = () => <Form className="my-form" schema={schema} />;
+const uiSchema = {
+    "ui:order": ["bar", "foo"],
+};
+
+const onChange = (e: IChangeEvent) => {
+    console.log(e);
+};
+
+const onSubmit = (e: ISubmitEvent<any>) => {
+    console.log(e);
+};
+
+const FormLayout: React.FC = () => (
+    <Form className="my-form" schema={schema} uiSchema={uiSchema} onChange={onChange} onSubmit={onSubmit} />
+);
 
 export { FormLayout };
