41d6132dc797c07a666caf88c16d05b0b3943bbe
makey <vadimmakievsky@gmail.com>
2021-11-29 17:15:10 +0300

    form-1: изм разметка для чекбокса

--- src/form-generator/checkbox/CheckboxWidget.tsx
+++ src/form-generator/checkbox/CheckboxWidget.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 // import WidgetProps from "@rjsf/core";
-import "./CheckboxWidget.scss";
 
 const CheckboxWidget: React.FC<any /*TODO: WidgetProps*/> = props => {
     const { required, value, schema } = props;
 
     return (
         <div className="my-form-checkbox">
-            <input className="my-form-checkbox__input" type="checkbox" value={value} required={required} />
-            <label className="my-form-checkbox__label"></label>
-            <span>{schema.title}</span>
+            <label>
+                <input className="my-form-checkbox__input" type="checkbox" value={value} required={required} />
+                <span className="my-form-checkbox__icon" />
+                <span className="my-form-checkbox__label">{schema.title}</span>
+            </label>
         </div>
     );
 };
