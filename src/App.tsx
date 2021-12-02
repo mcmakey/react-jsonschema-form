05bc83d11ec8771140f92f296a3f5f743f07707d
makey <vadimmakievsky@gmail.com>
2021-12-02 14:31:25 +0300

    form -1: add mui + emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
 import { FormLayout } from "./components/FormLayout";
+// import { MuiButton } from "./components/MuiButton";
+// import { MuiEmotionComponent } from "./components/MuiEmotionComponent";
 import "./App.css";
 
 function App() {
-    return <FormLayout />;
+    return (
+        <>
+            <FormLayout />
+            {/*
+                <MuiEmotionComponent />
+                <MuiButton />
+            */}
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+const MuiButton: React.FC = () => (
+    <Button variant="contained" color="primary">
+        MuiButton
+    </Button>
+);
+
+export { MuiButton };
