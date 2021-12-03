579de4c66df06b39c53ea7567ca01d9c00d5d07a
makey <vadimmakievsky@gmail.com>
2021-12-03 18:31:26 +0300

    form-1:  BaseInput - mui + emotion styled

--- src/form-generator/baseinput/BaseInput.tsx
+++ src/form-generator/baseinput/BaseInput.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
+import InputBase, { InputBaseProps } from "@mui/material/InputBase";
+import Icon from "@mui/material/Icon";
 // import WidgetProps from "@rjsf/core";
-import "./BaseInput.scss";
+import styled from "@emotion/styled";
+
+const Input: React.FC<InputBaseProps> = props => <InputBase {...props} endAdornment="ок" />;
+
+const StyledInput = styled(Input)`
+    padding: 16px;
+    font-size: 18px;
+    line-height: 1.33em;
+    border-radius: 3px;
+    border: none;
+    background-color: #f6f4f1;
+
+    .MuiInputBase-input {
+        padding: 0;
+    }
+`;
 
 const BaseInput: React.FC<any /*TODO: WidgetProps*/> = props => {
     const { required, value } = props;
 
-    return <input className="my-form-input" value={value} required={required} />;
+    return <StyledInput value={value} required={required} />;
 };
 
 export { BaseInput };
