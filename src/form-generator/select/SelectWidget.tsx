22fd1bc67f4fad5bc15534cad0ee2c8006291481
makey <vadimmakievsky@gmail.com>
2021-12-08 19:04:44 +0300

    form-1: styled select

--- src/form-generator/select/SelectWidget.tsx
+++ src/form-generator/select/SelectWidget.tsx
@@ -1,24 +1,51 @@
-import { Select, SelectProps, Input, InputProps, MenuItem } from "@mui/material";
+import { Select, SelectProps, Input, MenuItem } from "@mui/material";
 import styled from "@emotion/styled";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-const StyledSelect = styled(Select)<SelectProps>`
-    flex-grow: 1;
-    padding: 16px;
-    font-size: 18px;
-    line-height: 1.33em;
-    border-radius: 3px;
-    border: none;
-    background-color: #f6f4f1;
-`;
+// const StyledSelect = styled(Select)<SelectProps>`
+//     flex-grow: 1;
+//     padding: 16px;
+//     font-size: 18px;
+//     line-height: 1.33em;
+//     border-radius: 3px;
+//     border: none;
+//     background-color: #f6f4f1;
+// `;
 
-const StyledInput = styled(Input)<InputProps>`
-    border: none;
-`;
+const StyledSelect = styled(Select)<SelectProps>(({ theme }) => ({
+    flexGrow: "1",
+    padding: "16px",
+    fontSize: "18px",
+    lineHeight: "1.33em",
+    borderRadius: "3px",
+    border: "none",
+    backgroundColor: "#f6f4f1",
 
-const StyledArrowBackIosIcon = styled(ArrowBackIosIcon)`
-    transform: scale(0.6) rotate(-90deg);
-`;
+    "&:before": {
+        borderBottom: "none !important",
+
+        "&:hover": {
+            borderBottom: "none !important",
+        },
+    },
+
+    "&:after": {
+        borderBottom: "none !important",
+    },
+
+    "& .MuiSelect-select": {
+        paddingTop: "0",
+        paddingBottom: "0",
+    },
+}));
+
+const StyledArrowBackIosIcon = styled(ArrowBackIosIcon)(({ theme }) => ({
+    transform: "scale(0.6) rotate(-90deg)",
+
+    "&.MuiSelect-iconOpen": {
+        transform: "scale(0.6) rotate(90deg)",
+    },
+}));
 
 const SelectWidget: React.FC<any /*TODO: WidgetProps*/> = props => {
     const { value, options, schema, onChange } = props;
@@ -27,7 +54,7 @@ const SelectWidget: React.FC<any /*TODO: WidgetProps*/> = props => {
         <StyledSelect
             name={schema.title}
             value={value}
-            input={<StyledInput name={schema.title} id={`${schema.title}-helper`} />}
+            input={<Input name={schema.title} id={`${schema.title}-helper`} />}
             onChange={event => onChange(event.target.value)}
             IconComponent={StyledArrowBackIosIcon}
         >
