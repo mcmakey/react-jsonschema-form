ee9f6f7732e34d92ca324823ae51c4210d972feb
makey <vadimmakievsky@gmail.com>
2021-12-08 12:21:51 +0300

    form-1: styled radio-switch

--- src/form-generator/radio/RadioWidget.tsx
+++ src/form-generator/radio/RadioWidget.tsx
@@ -1,18 +1,48 @@
 import * as React from "react";
-import { RadioGroup, Radio, FormControlLabel } from "@mui/material";
-// import WidgetProps from "@rjsf/core";
+import { RadioGroup, RadioGroupProps, Radio, RadioProps, FormControlLabel } from "@mui/material";
 import "./RadioWidget.scss";
+import styled from "@emotion/styled";
+
+const StyledRadioGroup = styled(RadioGroup)<RadioGroupProps>(({ theme }) => ({
+    backgroundColor: "#f6f4f1",
+    padding: "8px",
+
+    "& .MuiFormControlLabel-root": {
+        marginRight: "0",
+        marginLeft: "0",
+
+        "& .MuiFormControlLabel-label": {
+            padding: "8px 16px",
+            borderRadius: "3px",
+        },
+    },
+}));
+
+const StyledRadio = styled(Radio)<RadioProps>(({ theme }) => ({
+    display: "none",
+
+    "&.Mui-checked + .MuiFormControlLabel-label": {
+        backgroundColor: "white",
+    },
+}));
 
 const RadioWidget: React.FC<any /*TODO: WidgetProps*/> = props => {
     const { options } = props;
 
     return (
-        <RadioGroup>
+        <StyledRadioGroup row>
             {options &&
-                options.enumOptions.map((option: any) => {
-                    return <FormControlLabel value={option.value} label={option.label} control={<Radio />} />;
+                options.enumOptions.map((option: any, index: any) => {
+                    return (
+                        <FormControlLabel
+                            value={option.value}
+                            label={option.label}
+                            control={<StyledRadio />}
+                            key={index}
+                        />
+                    );
                 })}
-        </RadioGroup>
+        </StyledRadioGroup>
     );
 };
 
